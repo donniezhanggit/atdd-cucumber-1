002924ec1078a7139780cf4151f289e69f57e368
Max Guernsey, III <max@hexsw.com>
2015-11-06 02:26:45 -0800

    Adding pom.xml and restructuring project to make Maven happy.

---
+++ src/main/java/Temperature.java
@@ -0,0 +1,5 @@
+public class Temperature {
+  public static int convertCelsiusToFahrenheit(int celsius) {
+    return 32;
+  }
+}
---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions { 
+  int celsius = -1; 
+  int fahrenheit = -1; 
+  @Given("^Celsius is (\\d+)$")
+  public void celsius_is(int arg1) throws Throwable {
+    celsius = arg1; 
+  }
+
+  @When("^converted to Fahrenheit$")
+  public void converted_to_Fahrenheit() throws Throwable {
+    fahrenheit = Temperature.convertCelsiusToFahrenheit(celsius); 
+  }
+
+  @Then("^result is (\\d+)$")
+  public void result_is(int arg1) throws Throwable {
+    assertEquals(arg1, fahrenheit); 
+  }
+}
